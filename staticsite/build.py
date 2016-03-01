ffb4ff1538c4d130d420454f2993c08aeb7ada3b
Enrico Zini <enrico@enricozini.org>
2016-03-01 16:59:36 +0100

    An attempt at parallelisation, just to measure if it would help

--- staticsite/build.py
+++ staticsite/build.py
@@ -29,14 +29,66 @@ class Build(SiteCommand):
                 os.unlink(abs)
 
     def write(self, site):
+        """
+        Generate output
+        """
         # Clear the target directory, but keep the root path so that a web
         # server running on it does not find itself running nowhere
         outdir = os.path.join(self.root, "web")
         if os.path.exists(outdir):
             self.clear_outdir(outdir)
 
-        # Generate output
-        for page in site.pages.values():
+        # cpu_count = os.cpu_count()
+        # if cpu_count > 1:
+        #     self.write_multi_process(site, cpu_count)
+        # else:
+        #     self.write_single_process(site)
+
+        # I tried trivial parallelisation with child processes but the benefits
+        # do not seem significant:
+        #
+        #     real  0m5.111s
+        #     user  0m8.096s
+        #     sys   0m0.644s
+        #
+        # compared with:
+        #
+        #     real  0m6.251s
+        #     user  0m5.760s
+        #     sys   0m0.468s
+        self.write_single_process(site)
+
+    def write_multi_process(self, site, child_count):
+        log.info("Generating pages using %d child processes", child_count)
+
+        pages = list(site.pages.values())
+
+        # From http://code.activestate.com/recipes/576785-partition-an-iterable-into-n-lists/
+        chunks = [pages[i::child_count] for i in range(child_count)]
+
+        print(len(pages))
+        for c in chunks:
+            print(len(c))
+
+        import sys
+        pids = set()
+        for chunk in chunks:
+            pid = os.fork()
+            if pid == 0:
+                self.write_pages(chunk)
+                sys.exit(0)
+            else:
+                pids.add(pid)
+
+        while pids:
+            (pid, status) = os.wait()
+            pids.discard(pid)
+
+    def write_single_process(self, site):
+        self.write_pages(site.pages.values())
+
+    def write_pages(self, pages):
+        for page in pages:
             for relpath, rendered in page.render().items():
                 dst = self.output_abspath(relpath)
                 rendered.write(dst)
