9396951e31fe0552742226812487fbd1275de593
Enrico Zini <enrico@enricozini.org>
2016-03-01 13:23:16 +0100

    Python packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+import sys
+
+setup(
+    name = "staticsite",
+    requires=[ 'unidecode', 'markdown', 'toml', 'yaml', 'jinja2', 'dateutil', 'livereload' ],
+    version = "0.1",
+    description = "Static site generator",
+    author = ["Enrico Zini"],
+    author_email = ["enrico@enricozini.org"],
+    url = "https://github.com/spanezz/staticsite",
+    license = "http://www.gnu.org/licenses/gpl-3.0.html",
+    packages = ["staticsite", "staticsite.slugify"],
+    scripts = ['ssite']
+)
