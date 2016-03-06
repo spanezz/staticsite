4da3228d35fa312a7e0cadf3d1e70fb813a6c863
Enrico Zini <enrico@enricozini.org>
2016-03-06 21:36:52 +0100

    Do the right thing with existing index.html files

--- staticsite/asset.py
+++ staticsite/asset.py
@@ -11,13 +11,19 @@ class Asset(Page):
     TYPE = "asset"
 
     def __init__(self, site, root_abspath, relpath):
+        dirname, basename = os.path.split(relpath)
+        if basename == "index.html":
+            linkpath = dirname
+        else:
+            linkpath = relpath
+
         super().__init__(
             site=site,
             root_abspath=root_abspath,
             src_relpath=relpath,
-            src_linkpath=relpath,
+            src_linkpath=linkpath,
             dst_relpath=relpath,
-            dst_link=os.path.join(settings.SITE_ROOT, relpath))
+            dst_link=os.path.join(settings.SITE_ROOT, linkpath))
         self.title = os.path.basename(relpath)
 
     def read_metadata(self):
