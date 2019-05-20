0a8f47a22165afdd0285b809fee68e877d5a253a
Enrico Zini <enrico@enricozini.org>
2019-05-20 18:28:32 +0200

    flake8 updates

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
-import sys
 
 setup(
-    name = "staticsite",
-    install_requires=[ 'unidecode', 'markdown', 'toml', 'PyYAML', 'jinja2', 'python_dateutil', 'python_slugify', 'pytz' ],
+    name="staticsite",
+    install_requires=['unidecode', 'markdown', 'toml', 'PyYAML', 'jinja2', 'python_dateutil', 'python_slugify', 'pytz'],
     # http://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-extras-optional-features-with-their-own-dependencies
     extras_require={
         'serve': ['livereload'],
     },
-    version = "0.5",
-    description = "Static site generator",
-    author = ["Enrico Zini"],
-    author_email = ["enrico@enricozini.org"],
-    url = "https://github.com/spanezz/staticsite",
-    license = "http://www.gnu.org/licenses/gpl-3.0.html",
-    packages = ["staticsite"],
-    scripts = ['ssite']
+    version="0.5",
+    description="Static site generator",
+    author=["Enrico Zini"],
+    author_email=["enrico@enricozini.org"],
+    url="https://github.com/spanezz/staticsite",
+    license="http://www.gnu.org/licenses/gpl-3.0.html",
+    packages=["staticsite"],
+    scripts=['ssite']
 )
