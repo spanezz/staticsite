35ea7d47e58b99ba5b6cfb8324f11d1455acf088
Enrico Zini <enrico@enricozini.org>
2019-11-23 14:44:45 +0100

    Export File in staticsite namespace

--- staticsite/__init__.py
+++ staticsite/__init__.py
@@ -1,6 +1,7 @@
 from .feature import Feature
 from .page import Page
 from .site import Site
+from .file import File
 
 
-__all__ = ("Feature", "Page", "Site")
+__all__ = ("Feature", "Page", "Site", "File")
