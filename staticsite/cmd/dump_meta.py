5991353bf852bd87951dc7e5a274a150f83c09ce
Enrico Zini <enrico@enricozini.org>
2019-12-15 13:59:22 +0100

    Allow to filter which pages are printed by dump_meta

--- staticsite/cmd/dump_meta.py
+++ staticsite/cmd/dump_meta.py
@@ -1,5 +1,5 @@
 from .command import SiteCommand
-from staticsite.utils import write_front_matter
+from staticsite.utils import write_front_matter, compile_page_match
 import sys
 import logging
 
@@ -16,12 +16,17 @@ class DumpMeta(SiteCommand):
         parser = super().make_subparser(subparsers)
         parser.add_argument("-f", "--format", action="store", default="yaml",
                             help="format to use for output")
+        parser.add_argument("-p", "--pages", nargs="+", default=(),
+                            help="globs or regexps matching pages to show")
         return parser
 
     def run(self):
         site = self.load_site()
+        filters = [compile_page_match(f) for f in self.args.pages]
         res = {}
         # show_repr = self.args.repr
         for relpath, page in sorted(site.pages.items()):
+            if filters and not any(f.match(relpath) for f in filters):
+                continue
             res[f"/{relpath}"] = page.to_dict()
         sys.stdout.write(write_front_matter(res, self.args.format))
