081596e0d9645e577f46369f5d89489fa4e25d40
Enrico Zini <enrico@enricozini.org>
2019-12-25 08:37:30 +0100

    Update a page date to be at least as recent as the most recent page in its pages list

--- staticsite/features/pages.py
+++ staticsite/features/pages.py
@@ -41,6 +41,11 @@ See [Selecting pages](page-filter.md) for details.
             # Do not include self in the result list
             pages = [p for p in f.filter(self.site.pages.values()) if p != page]
             page.meta["pages"] = pages
+            if pages:
+                max_date = max(p.meta["date"] for p in pages)
+
+            # Update the page date to the max of the pages dates
+            page.meta["date"] = max(max_date, page.meta["date"])
 
 
 FEATURES = {
