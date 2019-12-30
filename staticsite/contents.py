288b6a29fbbb6a5cb4c44a0914a311795bce321e
Enrico Zini <enrico@enricozini.org>
2019-12-30 12:46:40 +0100

    Scan asset dirs as assets, without involving features

--- staticsite/contents.py
+++ staticsite/contents.py
@@ -93,6 +93,60 @@ class Dir(Page):
         else:
             return io.open(src.abspath, *args, **kw)
 
+    def finalize(self):
+        # Finalize from the bottom up
+        for subdir in self.subdirs:
+            subdir.finalize()
+
+        # TODO if self.src.relpath:
+        # TODO     self.meta["title"] = os.path.basename(self.src.relpath)
+        # TODO elif self.site.settings.SITE_NAME:
+        # TODO     # If src_relpath is empty, we are the toplevel directory index
+        # TODO     self.meta["title"] = self.site.settings.SITE_NAME
+        # TODO else:
+        # TODO     # If we have no site name and we need to generate the toplevel
+        # TODO     # directory index, pick a fallback title.
+        # TODO     self.meta["title"] = os.path.dirname(self.site.content_root)
+
+        self.meta["pages"] = [p for p in self.pages if not p.meta["draft"]]
+        self.meta.setdefault("template", "dir.html")
+        self.meta["build_path"] = os.path.join(self.meta["site_path"], "index.html")
+
+        self.meta["indexed"] = bool(self.meta["pages"]) or any(p.meta["indexed"] for p in self.subdirs)
+
+        # TODO: set draft if all subdirs and pages are drafts
+
+        # Since finalize is called from the bottom up, subdirs have their date
+        # up to date
+        self.subdirs.sort(key=lambda p: p.meta["date"])
+        self.meta["pages"].sort(key=lambda p: p.meta["date"])
+
+        date_pages = []
+        if self.subdirs:
+            date_pages.append(self.subdirs[-1].meta["date"])
+        if self.meta["pages"]:
+            date_pages.append(self.meta["pages"][-1].meta["date"])
+
+        if date_pages:
+            self.meta["date"] = max(date_pages)
+        else:
+            self.meta["date"] = self.site.localized_timestamp(self.src.stat.st_mtime)
+
+        if self.meta["indexed"] and self.meta["site_path"] not in self.site.pages:
+            self.site.add_page(self)
+
+    def validate(self):
+        try:
+            super().validate()
+        except PageValidationError as e:
+            log.error("%s: infrastructural page failed to validate: %s", e.page, e.msg)
+            raise
+
+
+class ContentDir(Dir):
+    """
+    Content path which uses features for content loading
+    """
     @with_dir_fd
     def scan(self, dir_fd: int):
         # Scan directory contents
@@ -177,60 +231,6 @@ class Dir(Page):
         #       or hook into features here to keep track of the pages they want
         #       to load, and later just call the load method of each feature
 
-    def finalize(self):
-        # Finalize from the bottom up
-        for subdir in self.subdirs:
-            subdir.finalize()
-
-        # TODO if self.src.relpath:
-        # TODO     self.meta["title"] = os.path.basename(self.src.relpath)
-        # TODO elif self.site.settings.SITE_NAME:
-        # TODO     # If src_relpath is empty, we are the toplevel directory index
-        # TODO     self.meta["title"] = self.site.settings.SITE_NAME
-        # TODO else:
-        # TODO     # If we have no site name and we need to generate the toplevel
-        # TODO     # directory index, pick a fallback title.
-        # TODO     self.meta["title"] = os.path.dirname(self.site.content_root)
-
-        self.meta["pages"] = [p for p in self.pages if not p.meta["draft"]]
-        self.meta.setdefault("template", "dir.html")
-        self.meta["build_path"] = os.path.join(self.meta["site_path"], "index.html")
-
-        self.meta["indexed"] = bool(self.meta["pages"]) or any(p.meta["indexed"] for p in self.subdirs)
-
-        # TODO: set draft if all subdirs and pages are drafts
-
-        # Since finalize is called from the bottom up, subdirs have their date
-        # up to date
-        self.subdirs.sort(key=lambda p: p.meta["date"])
-        self.meta["pages"].sort(key=lambda p: p.meta["date"])
-
-        date_pages = []
-        if self.subdirs:
-            date_pages.append(self.subdirs[-1].meta["date"])
-        if self.meta["pages"]:
-            date_pages.append(self.meta["pages"][-1].meta["date"])
-
-        if date_pages:
-            self.meta["date"] = max(date_pages)
-        else:
-            self.meta["date"] = self.site.localized_timestamp(self.src.stat.st_mtime)
-
-        if self.meta["indexed"] and self.meta["site_path"] not in self.site.pages:
-            self.site.add_page(self)
-
-    def validate(self):
-        try:
-            super().validate()
-        except PageValidationError as e:
-            log.error("%s: infrastructural page failed to validate: %s", e.page, e.msg)
-            raise
-
-
-class ContentDir(Dir):
-    """
-    Content path which uses features for content loading
-    """
     @with_dir_fd
     def load(self, dir_fd: int):
         """
@@ -289,6 +289,49 @@ class AssetDir(ContentDir):
     Content path which loads everything as assets
     """
     @with_dir_fd
+    def scan(self, dir_fd: int):
+        # Scan directory contents
+        subdirs = {}
+        with os.scandir(self.dir_fd) as entries:
+            for entry in entries:
+                if entry.name.startswith("."):
+                    # Skip hidden directories
+                    continue
+
+                # Note: is_dir, is_file, and stat, follow symlinks by default
+                if entry.is_dir():
+                    # Take note of directories
+                    subdirs[entry.name] = file.File(
+                            relpath=os.path.join(self.src.relpath, entry.name),
+                            abspath=os.path.join(self.src.abspath, entry.name),
+                            stat=entry.stat())
+                else:
+                    # Take note of files
+                    self.files[entry.name] = file.File(
+                            relpath=os.path.join(self.src.relpath, entry.name),
+                            abspath=os.path.join(self.src.abspath, entry.name),
+                            stat=entry.stat())
+
+        # TODO: build a directory page here? Or at site load time?
+        # TODO: its contents can be added at analyze time
+
+        # Scan subdirectories
+        for name, src in subdirs.items():
+            # Compute metadata for this directory
+            meta: Meta = dict(self.meta)
+            meta["site_path"] = os.path.join(meta["site_path"], name)
+
+            # Recursively descend into the directory
+            subdir = Dir.create(self.site, src, meta=meta, dir=self)
+            with open_dir_fd(name, dir_fd=dir_fd) as subdir_fd:
+                subdir.scan(subdir_fd)
+            self.subdirs.append(subdir)
+
+        # TODO: build (but not load) pages here?
+        #       or hook into features here to keep track of the pages they want
+        #       to load, and later just call the load method of each feature
+
+    @with_dir_fd
     def load(self, dir_fd: int):
         """
         Read static assets from this directory and all its subdirectories
@@ -305,7 +348,7 @@ class AssetDir(ContentDir):
         for fname, f in self.files.items():
             if stat.S_ISREG(f.stat.st_mode):
                 log.debug("Loading static file %s", f.relpath)
-                meta = self.file_meta[fname]
+                meta = dict(self.meta)
                 meta["site_path"] = os.path.join(meta["site_path"], fname)
                 p = Asset(self.site, f, meta=meta, dir=self)
                 self.site.add_page(p)
