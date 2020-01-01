ed0d3e05614b7cc9a3e7cdbf8646f4c979cf3204
Enrico Zini <enrico@enricozini.org>
2020-01-01 11:38:52 +0100

    Cleanup to image metadata editing

--- staticsite/cmd/meta.py
+++ staticsite/cmd/meta.py
@@ -3,6 +3,7 @@ from .command import Command, Fail, Success
 from staticsite.utils import images
 from staticsite.cache import DisabledCache
 from staticsite.utils import yaml_codec as yaml
+from staticsite.utils.typing import Meta
 import shlex
 import tempfile
 import logging
@@ -14,7 +15,14 @@ class Meta(Command):
     """
     Edit metadata for a file
     """
-    def edit(self, fname):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        self.scanner = images.ImageScanner(DisabledCache())
+
+    def edit(self, fname: str) -> subprocess.CompletedProcess:
+        """
+        Run the editor on this file
+        """
         settings_dict = self.settings.as_dict()
         cmd = [x.format(name=fname, **settings_dict) for x in self.settings.EDIT_COMMAND]
         try:
@@ -24,24 +32,22 @@ class Meta(Command):
                 " ".join(shlex.quote(x) for x in cmd), e.returncode))
         return res
 
-    def run(self):
-        # TODO: Build a Site if possible
-        # TODO: or load settings from a settings.py if one can be found in reasonable places
-        scanner = images.ImageScanner(DisabledCache())
-        meta = scanner.scan_file(self.args.file)
+    def edit_meta(self, meta: Meta) -> Meta:
+        """
+        Edit the given metadata in an editor
+        """
         with tempfile.NamedTemporaryFile(suffix=".yaml") as fd:
             yaml.dump(meta, fd)
             fd.flush()
-            # TODO: filter the keys that we don't need to edit, like width and height
-            # TODO: setdefault the keys that are relevant and might not be there
             self.edit(fd.name)
             with open(fd.name, "rt") as newfd:
-                new_meta = yaml.load(newfd)
+                return yaml.load(newfd)
 
-        # TODO: set in file
+    def save_changes(self, old_meta: Meta, new_meta: Meta):
+        # Compute and store the changes
         changed = {}
         removed = []
-        for key, orig in meta.items():
+        for key, orig in old_meta.items():
             if key not in new_meta:
                 log.info("%s: removed %s", self.args.file, key)
                 removed.append(key)
@@ -49,15 +55,37 @@ class Meta(Command):
                 log.info("%s: updated %s=%r", self.args.file, key, new_meta[key])
                 changed[key] = new_meta[key]
 
-        for key in new_meta.keys() - meta.keys():
+        for key in new_meta.keys() - old_meta.keys():
             log.info("%s: added %s=%r", self.args.file, key, new_meta[key])
             changed[key] = new_meta[key]
 
         if not changed and not removed:
             raise Success()
 
-        if not scanner.edit_meta_exiftool(self.args.file, changed, removed):
+        if not self.scanner.edit_meta_exiftool(self.args.file, changed, removed):
             raise Fail("Failed to store metadatä changes")
+
+    def run(self):
+        # TODO: Build a Site if possible
+        # TODO: or load settings from a settings.py if one can be found in reasonable places
+
+        # Read file metadata
+        meta = self.scanner.scan_file(self.args.file)
+
+        # Filter the keys that we don't need to edit
+        meta.pop("width", None)
+        meta.pop("height", None)
+        meta.pop("lat", None)
+        meta.pop("lon", None)
+
+        # Set the keys that are relevant and might not be there to default values
+        meta.setdefault("title", "")
+        meta.setdefault("author", "")
+
+        # Edit the metadata
+        new_meta = self.edit_meta(meta)
+
+        self.save_changes(meta, new_meta)
 
     @classmethod
     def make_subparser(cls, subparsers):
