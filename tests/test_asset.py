68ca0192bef24acf941e282af25499816f97a421
Enrico Zini <enrico@enricozini.org>
2022-11-30 01:08:23 +0100

    Fixed test_asset

--- tests/test_asset.py
+++ tests/test_asset.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 from . import utils as test_utils
-from staticsite.asset import Asset
 from staticsite.file import File
 from contextlib import contextmanager
 import os
@@ -37,16 +36,17 @@ class TestAsset(TestCase):
     @test_utils.assert_no_logs()
     def test_timestamps(self):
         site = test_utils.Site()
-        dir = Dir(site, None, {"site_path": "/", "site_url": "https://www.example.org"}, None, None)
+        site.structure.root.meta["site_url"] = "https://www.example.org"
 
         with override_tz("Pacific/Samoa"):
             with tempfile.NamedTemporaryFile() as f:
                 # $ TZ=UTC date +%s --date="2016-11-01" → 1477958400
                 os.utime(f.name, (1477958400, 1477958400))
                 src = File(os.path.basename(f.name), abspath=f.name, stat=os.stat(f.name))
-                page = Asset(site, src, meta={}, dir=dir, name=f.name)
+                page = site.structure.root.add_asset(src=src, name="testasset")
 
                 self.assertEqual(page.meta["date"], datetime.datetime(2016, 11, 1, 0, 0, 0, tzinfo=pytz.utc))
-                self.assertEqual(page.meta["site_path"], f.name)
                 self.assertEqual(page.meta["site_url"], "https://www.example.org")
-                self.assertEqual(page.meta["build_path"], f.name.lstrip("/"))
+                self.assertEqual(page.site_path, "testasset")
+                self.assertEqual(page.build_path, "testasset")
+                self.assertFalse(page.directory_index)
