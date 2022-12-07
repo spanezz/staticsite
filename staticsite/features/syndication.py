5990f7883812a081df77eabe2d44c5756db0ed27
Enrico Zini <enrico@enricozini.org>
2022-12-08 00:34:16 +0100

    Ported syndication away from metadata registry

--- staticsite/features/syndication.py
+++ staticsite/features/syndication.py
@@ -8,44 +8,86 @@ import jinja2
 
 from staticsite import structure
 from staticsite.feature import Feature
-from staticsite.metadata import Metadata
+from staticsite.metadata import FieldsMetaclass, Metadata
 from staticsite.page import Page, PageNotFoundError
 from staticsite.utils import arrange
 
 log = logging.getLogger("syndication")
 
 
-class MetadataSyndicated(Metadata):
-    """
-    Make sure the syndicated exists
-    """
-    def on_load(self, page: Page):
-        val = page.meta.get(self.name)
-        if val is None:
-            # If not present, default to 'indexed'
-            page.meta[self.name] = page.meta["indexed"]
-        elif isinstance(val, str):
-            page.meta[self.name] = val.lower() in ("yes", "true", "1")
-
-
-class MetadataSyndicationDate(Metadata):
-    """
-    Parse to a date if provided.
-
-    If page.meta.syndicate is true, it is always present, and if not set it
-    defaults to page.meta.date.
-    """
-    def on_load(self, page: Page):
-        date = page.meta.get(self.name)
-        if date is None:
-            if page.meta["syndicated"]:
-                page.meta[self.name] = page.meta["date"]
-        else:
-            page.meta[self.name] = self.site.clean_date(date)
-
-
 class SyndicatedPageError(Exception):
     pass
+
+
+class SyndicationPageMixin(metaclass=FieldsMetaclass):
+    syndication = Metadata(structure=True, doc="""
+        Defines syndication for the contents of this page.
+
+        It is a structure which can contain normal metadata, plus:
+
+        * `add_to`: chooses which pages will include a link to the RSS/Atom feeds.
+          By default, the link is added to all syndicated pages. If this is `False`,
+          then no feed is added to pages. If it is a dictionary, it selects pages in
+          the site, similar to the `site_pages` function in [templates](templates.md).
+          See [Selecting pages](page-filter.md) for details.
+        * `archive`: if not false, an archive link will be generated next to the
+          page. It can be set of a dictionary of metadata to be used as defaults for
+          the generated archive page. It defaults to True.
+
+        Any other metadata found in the structure are used when generating pages for
+        the RSS/Atom feeds, so you can use `title`, `template_title`, `description`,
+        and so on, to personalize the feeds.
+
+        The pages that go in the feed are those listed in
+        [`page.meta.pages`](doc/reference/pages.md), keeping into account the
+        [`syndicated` and `syndication_date` page metadata](doc/reference/metadata.md).
+
+        When rendering RSS/Atom feed pages, `page.meta.pages` is replaced with the list
+        of syndicated pages, sorted with the most recent first.
+
+        Setting `syndication` to true turns on syndication with all defaults,
+        equivalent to:
+
+        ```yaml
+        syndication:
+          add_to: yes
+          archive:
+            template: archive.html
+        ```
+    """)
+    syndicated = Metadata(doc="""
+        Set to true if the page can be included in a syndication, else to false.
+
+        If not set, it defaults to the value of `indexed`.
+    """)
+    syndication_date = Metadata(doc="""
+        Syndication date for this page.
+
+        This is the date that will appear in RSS and Atom feeds, and the page will not
+        be syndicated before this date.
+
+        If a page is syndicated and `syndication_date` is missing, it defaults to `date`.
+    """)
+
+    def validate(self):
+        super().validate()
+
+        # Make sure the syndicated exists
+        if (val := self.meta.get("syndicated")) is None:
+            # If not present, default to 'indexed'
+            self.meta["syndicated"] = self.meta["indexed"]
+        elif isinstance(val, str):
+            self.meta["syndicated"] = val.lower() in ("yes", "true", "1")
+
+        # Parse syndication_date to a date if provided.
+        #
+        # If page.meta.syndicate is true, it is always present, and if not set it
+        # defaults to page.meta.date.
+        if (date := self.meta.get("syndication_date")) is None:
+            if self.meta["syndicated"]:
+                self.meta["syndication_date"] = self.meta["date"]
+        else:
+            self.meta["syndication_date"] = self.site.clean_date(date)
 
 
 def _get_syndicated_pages(page: Page, limit: Optional[int] = None) -> List[Page]:
@@ -83,59 +125,10 @@ class SyndicationFeature(Feature):
 
     def __init__(self, *args, **kw):
         super().__init__(*args, **kw)
+        self.page_mixins.append(SyndicationPageMixin)
         self.site.features["rst"].yaml_tags.add("syndication")
-        self.site.register_metadata(Metadata("syndication", structure=True, doc="""
-Defines syndication for the contents of this page.
-
-It is a structure which can contain normal metadata, plus:
-
-* `add_to`: chooses which pages will include a link to the RSS/Atom feeds.
-  By default, the link is added to all syndicated pages. If this is `False`,
-  then no feed is added to pages. If it is a dictionary, it selects pages in
-  the site, similar to the `site_pages` function in [templates](templates.md).
-  See [Selecting pages](page-filter.md) for details.
-* `archive`: if not false, an archive link will be generated next to the
-  page. It can be set of a dictionary of metadata to be used as defaults for
-  the generated archive page. It defaults to True.
-
-Any other metadata found in the structure are used when generating pages for
-the RSS/Atom feeds, so you can use `title`, `template_title`, `description`,
-and so on, to personalize the feeds.
-
-The pages that go in the feed are those listed in
-[`page.meta.pages`](doc/reference/pages.md), keeping into account the
-[`syndicated` and `syndication_date` page metadata](doc/reference/metadata.md).
-
-When rendering RSS/Atom feed pages, `page.meta.pages` is replaced with the list
-of syndicated pages, sorted with the most recent first.
-
-Setting `syndication` to true turns on syndication with all defaults,
-equivalent to:
-
-```yaml
-syndication:
-  add_to: yes
-  archive:
-    template: archive.html
-```
-"""))
         self.site.structure.add_tracked_metadata("syndication")
-
-        self.site.register_metadata(MetadataSyndicated("syndicated", doc="""
-Set to true if the page can be included in a syndication, else to false.
-
-If not set, it defaults to the value of `indexed`.
-"""))
-        self.site.register_metadata(MetadataSyndicationDate("syndication_date", doc="""
-Syndication date for this page.
-
-This is the date that will appear in RSS and Atom feeds, and the page will not
-be syndicated before this date.
-
-If a page is syndicated and `syndication_date` is missing, it defaults to `date`.
-"""))
         self.syndications = []
-
         self.j2_globals["syndicated_pages"] = self.jinja2_syndicated_pages
 
     @jinja2.pass_context
