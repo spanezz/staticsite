a66b97445cabeef9205d69032fbb9bf9debaeea9
Enrico Zini <enrico@enricozini.org>
2022-12-08 01:37:17 +0100

    rst tests now also pass

--- tests/test_rst.py
+++ tests/test_rst.py
@@ -12,7 +12,7 @@ class TestRst(test_utils.MockSiteTestMixin, TestCase):
             "taxonomies/tags.taxonomy": {},
             "index.rst": """
 :date: 2016-04-16 10:23:00+02:00
-:tags: [example, "another tag"]
+:tags: [example, "another_tag"]
 :foo:
   line1
   line2
@@ -24,11 +24,26 @@ Example blog post in reStructuredText
         }
 
         with self.site(files) as mocksite:
-            page, tags, tag_example, tag_another = mocksite.page("", "taxonomies/tags", "example", "another tag")
+            mocksite.assertPagePaths((
+                "",
+                'taxonomies',
+                "taxonomies/tags",
+                'taxonomies/tags/example',
+                'taxonomies/tags/example/index.rss',
+                'taxonomies/tags/example/index.atom',
+                'taxonomies/tags/example/archive',
+                'taxonomies/tags/another_tag',
+                'taxonomies/tags/another_tag/index.rss',
+                'taxonomies/tags/another_tag/index.atom',
+                'taxonomies/tags/another_tag/archive',
+            ))
+            page, tags, tag_example, tag_another = mocksite.page(
+                    "", "taxonomies/tags",
+                    "taxonomies/tags/example", "taxonomies/tags/another_tag")
 
             # We have a root dir index and dir indices for all subdirs
             self.assertEqual(page.TYPE, "rst")
-            self.assertCountEqual(page.meta.pop("tags"), [tag_example, tag_another])
+            self.assertCountEqual(page.meta["tags"], [tag_example, tag_another])
             self.assertEqual(page.to_dict(), {
                 "src": {
                     "abspath": os.path.join(mocksite.site.content_root, "index.rst"),
@@ -44,7 +59,12 @@ Example blog post in reStructuredText
                     "syndicated": True,
                     "syndication_date": "2016-04-16 10:23:00+02:00",
                     "foo": "line1\nline2\nline3",
+                    'tags': [
+                        'CategoryPage(taxonomies/tags/example)',
+                        'CategoryPage(taxonomies/tags/another_tag)'
+                    ],
                     "template": "page.html",
+                    'template_copyright': 'compiled:None',
                     "title": "Example blog post in reStructuredText",
                     "indexed": True,
                     "site_name": "Test site",
