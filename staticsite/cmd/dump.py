c612fccdab7a26f7d79dd31005ee1c03cc5e9644
Enrico Zini <enrico@enricozini.org>
2022-12-16 15:55:09 +0100

    New command ssite dump --changes

--- staticsite/cmd/dump.py
+++ staticsite/cmd/dump.py
@@ -22,6 +22,8 @@ class Dump(SiteCommand):
                            help="dump information about the scanned directory trees")
         group.add_argument("--nodes", action="store_true",
                            help="dump information about the built site layout")
+        group.add_argument("--changes", action="store_true",
+                           help="dump information about pages that changed")
         return parser
 
     def dump_fstree(self, site: Site):
@@ -32,11 +34,19 @@ class Dump(SiteCommand):
     def dump_nodes(self, site: Site):
         site.root.print()
 
+    def dump_changes(self, site: Site):
+        for page in site.iter_pages():
+            change = page.change_extent
+            if change > 0:
+                print(f"{page!r} {change}")
+
     def run(self):
         site = self.load_site()
         if self.args.fstree:
             self.dump_fstree(site)
         elif self.args.nodes:
             self.dump_nodes(site)
+        elif self.args.changes:
+            self.dump_changes(site)
         else:
             raise Fail("I don't know what to do")
