87c717c47843a25098fcef5ca63a029790cae865
Enrico Zini <enrico@enricozini.org>
2022-12-17 15:55:01 +0100

    Moved jinja2 pages doc to docstrings

--- staticsite/features/jinja2.py
+++ staticsite/features/jinja2.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
-from typing import List, TYPE_CHECKING, Optional, Any
-from staticsite import node
-from staticsite.page import SourcePage, Page, ChangeExtent
-from staticsite.feature import Feature
-from staticsite.utils import front_matter
-from staticsite.page_filter import compile_page_match
+
+import logging
+from typing import TYPE_CHECKING, Any, List, Optional, Type
+
 import jinja2
 import markupsafe
-import logging
+
+from staticsite import node
+from staticsite.feature import Feature
+from staticsite.page import ChangeExtent, Page, SourcePage
+from staticsite.page_filter import compile_page_match
+from staticsite.utils import front_matter
 
 if TYPE_CHECKING:
     from staticsite import file, fstree
@@ -48,6 +51,9 @@ class J2Pages(Feature):
 
     See doc/reference/templates.md for details.
     """
+    def get_used_page_types(self) -> list[Type[Page]]:
+        return [J2Page]
+
     def load_dir_meta(self, directory: fstree.Tree) -> Optional[dict[str, Any]]:
         # Load front matter from index.html
         if (index := directory.files.get("index.html")) is None:
@@ -165,6 +171,41 @@ class RenderPartialTemplateMixin:
 
 
 class J2Page(RenderPartialTemplateMixin, SourcePage):
+    """
+    Jinja2 pages
+
+    You can put [jinja2 templates](templates.md) in your site contents, and they
+    will be rendered as site pages.
+
+    This can be used to generate complex index pages, blog front pages, and
+    anything else [Jinja2](http://jinja.pocoo.org/) is able to generate.
+
+    You can set `JINJA2_PAGES` in the [site settings](settings.md) to a list of
+    patterns (globs or regexps as in [page filters](page-filter.md)) matching file
+    names to consider jinja2 templates by default. It defaults to
+    `["*.html", "*.j2.*"]`.
+
+    Any file with `.j2.` in their file name will be rendered as a template,
+    stripping `.j2.` in the destination file name.
+
+    For example, `dir/robots.j2.txt` will become `dir/robots.txt` when the site is
+    built.
+
+    ## Front matter
+
+    If a page defines a jinja2 block called `front_matter`, the block is rendered
+    and parsed as front matter.
+
+    **Note**: [jinja2 renders all contents it finds before
+    `{% extends %}`](https://jinja.palletsprojects.com/en/2.10.x/templates/#child-template).
+    To prevent your front matter from ending up in the rendered HTML, place the
+    `front_matter` block after the `{% extends %}` directive, or manage your front
+    matter from [`.staticfile` directory metadata](content.md).
+
+    If you want to use `template_*` entries, you can wrap the front matter around
+    `{% raw %}` to prevent jinja2 from rendering their contents as part of the rest
+    of the template.
+    """
     TYPE = "jinja2"
 
     def __init__(self, *args, template: jinja2.Template, **kw):
